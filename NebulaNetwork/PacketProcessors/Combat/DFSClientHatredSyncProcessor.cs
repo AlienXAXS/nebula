9a8e515f3b43307a8d421056c931b52f53790a83
AlienXAXS <github@smtpd.co.uk>
2024-07-19 19:50:32 +0100

    Updated checks for EnemyPool and UnitBuffer

--- NebulaNetwork/PacketProcessors/Combat/DFSClientHatredSyncProcessor.cs
+++ NebulaNetwork/PacketProcessors/Combat/DFSClientHatredSyncProcessor.cs
@@ -15,19 +15,18 @@ namespace NebulaNetwork.PacketProcessors.Combat
             Log.Debug($"DFSClientHatredSyncPacket Adding {packet.Hatred} hatred to EnemyPool {packet.EnemyPoolId} via unit {packet.UnitComponentId}");
 
             var unitBuffer = GameMain.spaceSector.combatSpaceSystem.units.buffer;
-            if (!unitBuffer.Any(x => x.id.Equals(packet.UnitComponentId)))
+            if (GameMain.spaceSector.combatSpaceSystem.units.buffer[packet.UnitComponentId].id != packet.UnitComponentId)
             {
                 Log.Debug($"DFSClientHatredSyncPacket error: Unable to find UnitBuffer with ID {packet.UnitComponentId}");
                 return;
             }
 
-            var enemyPool = GameMain.spaceSector.enemyPool;
-            if (!enemyPool.Any(x => x.id.Equals(packet.EnemyPoolId)))
+            if (GameMain.spaceSector.enemyPool[packet.EnemyPoolId].id != packet.EnemyPoolId)
             {
                 Log.Debug($"DFSClientHatredSyncPacket error: Unable to find EnemyPool with ID {packet.EnemyPoolId}");
                 return;
             }
-
+            
             unitBuffer[packet.UnitComponentId].hatred.HateTarget(EObjectType.Enemy, packet.EnemyPoolId, packet.Hatred, 3000, EHatredOperation.Add);
         }
     }
